2dfce0470a0d05ace748c678fb2245c1c674211f
bruno-coulet <bruno.coulet@laplateforme.io>
2024-02-11 15:35:23 +0100

    gui_message, 2ème version, avec commentaires

--- gui_message.py
+++ gui_message.py
@@ -28,53 +28,92 @@ class Messagerie(customtkinter.CTk):
 
         self.title("Messagerie")
         self.geometry("600x700")
-        # give both columns an equal weight.
+        # gives an equal weight to column 0 and column 1 .
         self.grid_columnconfigure((0, 1), weight=1)
 
-        # Titre de la fenêtre
-        title_label = customtkinter.CTkLabel(self, text="Bienvenue dans la messagerie", font=("Helvetica", 16))
+
+        # ----  WINDOW TITLE - ROW 0     -------
+        title_label = customtkinter.CTkLabel(self, text="Bienvenue dans la messagerie", font=("Helvetica", 30))
         title_label.grid(row=0, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
         
 
-        self.button_create_channel = customtkinter.CTkButton(self, text="Créer un channel", command=create_channel)
-        self.button_create_channel.grid(row=1, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+        # ----  CHANNEL FRAME - ROW 1     -------
 
-        # sticky="ew"  --->   occupe toute la largeur de la cellule
-        self.button_send_message = customtkinter.CTkButton(self, text="Publier le message", command=lambda: print("send message", entry_text.get()))
-        self.button_send_message.grid(row=2, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+        self.channel_frame = customtkinter.CTkFrame(self)
+        self.channel_frame.grid(row=1, column=0, padx=10, pady=(10, 0))
+        # Configurer les colonnes du channel_frame pour qu'elles prennent chacune la moitié de la largeur
+        self.channel_frame.grid_columnconfigure((0, 1), weight=1)
 
-        # message text ou audio
-        self.checkbox_1 = customtkinter.CTkCheckBox(self, text="Message texte")
-        self.checkbox_1.grid(row=3, column=0, padx=20, pady=(20, 20), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self, text="Message audio")
-        self.checkbox_2.grid(row=3, column=1, padx=20, pady=(20, 20), sticky="w")
+        self.button_create_channel = customtkinter.CTkButton(self.channel_frame, text="Créer un autre channel", command=create_channel)
+        self.button_create_channel.grid(row=0, column=1, padx=20, pady=20, sticky="w")
 
-        # Zone de saisie de texte
-        entry_text = customtkinter.CTkEntry(self, width=400, height=100)
-        entry_text.grid(row=4, column=0, padx=10, pady=10)
+        channel_label = customtkinter.CTkLabel(self.channel_frame, text="Channel actuel : Les courges ont encore augmentées.", font=("Helvetica", 15))
+        channel_label.grid(row=0, column=0, padx=20, pady=20, sticky="e")
 
-        # Label
-        message_label = customtkinter.CTkLabel(self, text="Messages du channel.")
-        message_label.grid(row=5, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+        # existant messages - Label
+        old_messages_label = customtkinter.CTkLabel(self.channel_frame, text="Messages du channel.", font=("Helvetica", 15))
+        old_messages_label.grid(row=1, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
 
-        self.checkbox_frame = customtkinter.CTkFrame(self)
-        self.checkbox_frame.grid(row=6, column=0, padx=10, pady=(10, 0), sticky="nsw")
-        self.checkbox_1 = customtkinter.CTkCheckBox(self.checkbox_frame, text="message 1")
-        self.checkbox_1.grid(row=0, column=0, padx=10, pady=(10, 0), sticky="w")
-        self.checkbox_2 = customtkinter.CTkCheckBox(self.checkbox_frame, text="message 2")
-        self.checkbox_2.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
-
-
+        # existant messages - checkboxe for each
+        self.checkbox_1 = customtkinter.CTkCheckBox(self.channel_frame, text="message 1")
+        self.checkbox_1.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.checkbox_2 = customtkinter.CTkCheckBox(self.channel_frame, text="message 2")
+        self.checkbox_2.grid(row=3, column=0, padx=10, pady=(10, 0), sticky="w")
+        self.checkbox_3 = customtkinter.CTkCheckBox(self.channel_frame, text="message 3")
+        self.checkbox_3.grid(row=2, column=1, padx=10, pady=(10, 0), sticky="w")
+        self.checkbox_4 = customtkinter.CTkCheckBox(self.channel_frame, text="message 4")
+        self.checkbox_4.grid(row=3, column=1, padx=10, pady=(10, 0), sticky="w")
+        # ... prévoir ascenceur
 
 
+        # ----  NEW MESSAGE FRAME  - ROW 2    ---
+        
+        self.message_frame = customtkinter.CTkFrame(self)
+        self.message_frame.grid(row=2, column=0, padx=10, pady=(10, 0))
+        self.message_frame.grid_columnconfigure((0, 1), weight=1)
+        # New message - label
+        new_message_label = customtkinter.CTkLabel(self.message_frame, text="Nouveau Message.")
+        new_message_label.grid(row=0, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+        # new message - is either text or audio message
+        self.checkbox_1 = customtkinter.CTkCheckBox(self.message_frame, text="Message texte")
+        self.checkbox_1.grid(row=1, column=0, padx=20, pady=(20, 20), sticky="w")
+        self.checkbox_2 = customtkinter.CTkCheckBox(self.message_frame, text="Message audio")
+        self.checkbox_2.grid(row=1, column=1, padx=20, pady=(20, 20), sticky="w")
+        # new message - text input area
+        entry_text = customtkinter.CTkEntry(self.message_frame, width=400, height=100)
+        entry_text.grid(row=2, column=0, padx=10, pady=10)
+        # new message - send button
+        self.button_send_message = customtkinter.CTkButton(self.message_frame, text="Publier le message", command=lambda: print("send message", entry_text.get()))
+        self.button_send_message.grid(row=3, column=0, padx=20, pady=20, sticky="ew", columnspan=2)
+
+
+
+
+        # # ----OLD MESSAGE FRAME  - ROW 3    ---
+        
+        # self.old_message_frame = customtkinter.CTkFrame(self)
+        # self.old_message_frame.grid(row=3, column=0, padx=10, pady=(10, 0), sticky="nswe")
+        # self.old_message_frame.grid_columnconfigure((0, 1), weight=1)
+        # # existant messages - Label
+        # old_messages_label = customtkinter.CTkLabel(self.old_message_frame, text="Messages du channel.")
+        # old_messages_label.grid(row=0, column=0, padx=10, pady=10, sticky="ew", columnspan=2)
+
+        # # existant messages - checkboxe for each
+        # self.checkbox_1 = customtkinter.CTkCheckBox(self.old_message_frame, text="message 1")
+        # self.checkbox_1.grid(row=1, column=0, padx=10, pady=(10, 0), sticky="w")
+        # self.checkbox_2 = customtkinter.CTkCheckBox(self.old_message_frame, text="message 2")
+        # self.checkbox_2.grid(row=2, column=0, padx=10, pady=(10, 0), sticky="w")
+        # self.checkbox_3 = customtkinter.CTkCheckBox(self.old_message_frame, text="message 3")
+        # self.checkbox_3.grid(row=1, column=1, padx=10, pady=(10, 0), sticky="w")
+        # self.checkbox_4 = customtkinter.CTkCheckBox(self.old_message_frame, text="message 4")
+        # self.checkbox_4.grid(row=2, column=1, padx=10, pady=(10, 0), sticky="w")
+        # # ... prévoir ascenceur
+
+
+        #  log out - button
         self.button_log_out = customtkinter.CTkButton(self, text="Se déconnecter", command=log_out)
         self.button_log_out.grid(row=7 , column=0, padx=20, pady=20, sticky="ew", columnspan=2)
 
-        # column 0 spans the whole window because it has a weight of 1 and therefore expands.
-        # donc cela centre le bouton car la colonne fait toute la largeur de la fenêtre
-        # self.grid_columnconfigure(0, weight=1)
-
-        # self.grid_rowconfigure(0, weight=1)
 
 
 messagerie = Messagerie()
