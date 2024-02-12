9eeca3e01524c7df79496e4b4d07c18404fc52ad
lucas <lucas.savioz@laplateforme.io>
2024-02-12 18:05:41 +0100

    Modifications du rendu de la fenêtre login en cours

---
+++ constants.py
@@ -0,0 +1,24 @@
+#!/usr/bin/venv python3
+# -*- coding: utf-8 -*-
+"""
+@author: Bruno Coulet
+
+@file: constants.py
+@created: 12/02/2024
+
+@project: myDiscord
+@licence: GPLv3
+"""
+# 
+#  FONT
+# 
+TITLE_FONT=("helvetica", 25)
+SUBTITLE_FONT=("helvetica", 20)
+FONT=("helvetica", 15)
+
+# 
+#  COLORS
+# 
+BG_COLOR = "gray10"
+HIGHLIGHT_COLOR = "MediumPurple4"
+FG_COLOR = "MediumPurple1"
--- login.py
+++ login.py
@@ -1,73 +1,85 @@
+import customtkinter as ctk
 import tkinter as tk
+from constants import *
 
-class Login(tk.Tk):
+class Login(ctk.CTk):
     def __init__(self):
         super().__init__()
         self.title("Login")
-
-        # Création du cadre de connexion 
-        self.login_frame = tk.Frame(self, padx=600, pady=700)
-        # Taille de la fenêtre principale
         self.geometry("600x700")
-        # Centrage de la fenêtre principale
-        self.login_frame.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
-        ####### Paramètres des éléments visuel de la fenêtre Login #######
+        # Création du cadre de connexion avec la couleur de fond spécifiée et la taille définie
+        window_width = 600  # largeur de la fenêtre principale
+        window_height = 700  # hauteur de la fenêtre principale
+        self.login_frame = ctk.CTkFrame(self, bg_color=BG_COLOR, width=window_width, height=window_height)
+        self.login_frame.pack(fill="both", expand=True)
 
-        self.title_label = tk.Label(self.login_frame, text="Login", font=("Arial", 40))
+        
+        ####### Paramètres des éléments visuels de la fenêtre Login #######
+
+        self.title_label = ctk.CTkLabel(self.login_frame, text="Login", font=TITLE_FONT)
         self.title_label.grid(row=0, column=0, columnspan=2, pady=10)
+        self.login_frame.grid_columnconfigure(0, weight=1)  # Permet d'étirer la première colonne pour centrer le label
+        self.login_frame.grid_columnconfigure(1, weight=1)  # Permet d'étirer la deuxième colonne pour centrer le label
 
-        self.username_label = tk.Label(self.login_frame, text="Name:", font=("Arial", 12))
+
+        self.username_label = ctk.CTkLabel(self.login_frame, text="Name:", font=SUBTITLE_FONT)
         self.username_label.grid(row=1, column=0, padx=10, pady=5, sticky="e")
-        self.username_entry = self.create_entry(self.login_frame, "Name", font=("Arial", 12))
+        self.username_entry = self.create_entry(self.login_frame, "Name", font=SUBTITLE_FONT)
         self.username_entry.grid(row=1, column=1, padx=10, pady=5)
 
-        self.firstname_label = tk.Label(self.login_frame, text="Firstname:", font=("Arial", 12))
+        self.firstname_label = ctk.CTkLabel(self.login_frame, text="Firstname:", font=SUBTITLE_FONT)
         self.firstname_label.grid(row=2, column=0, padx=10, pady=5, sticky="e")
-        self.firstname_entry = self.create_entry(self.login_frame, "Firstname", font=("Arial", 12))
+        self.firstname_entry = self.create_entry(self.login_frame, "Firstname", font=SUBTITLE_FONT)
         self.firstname_entry.grid(row=2, column=1, padx=10, pady=5)
 
-        self.password_label = tk.Label(self.login_frame, text="Password:", font=("Arial", 12))
+        self.password_label = ctk.CTkLabel(self.login_frame, text="Password:", font=SUBTITLE_FONT)
         self.password_label.grid(row=3, column=0, padx=10, pady=5, sticky="e")
-        self.password_entry = self.create_entry(self.login_frame, "Password", font=("Arial", 12), show="*")
+        self.password_entry = self.create_entry(self.login_frame, "Password", font=SUBTITLE_FONT, show="*")
         self.password_entry.grid(row=3, column=1, padx=10, pady=5)
 
         # Bouton créer un compte
-        self.create_account_button = tk.Button(self.login_frame, text="Create account", font=("Arial", 12), command=self.create_account)
+        self.create_account_button = ctk.CTkButton(self.login_frame, text="Create account", font=SUBTITLE_FONT, bg_color=HIGHLIGHT_COLOR, command=self.create_account)
         self.create_account_button.grid(row=4, column=0, columnspan=2, padx=10, pady=5)
 
         # Bouton se connecter
-        self.login_button = tk.Button(self.login_frame, text="Login", font=("Arial", 12), command=self.login)
+        self.login_button = ctk.CTkButton(self.login_frame, text="Login", font=SUBTITLE_FONT, bg_color=HIGHLIGHT_COLOR, command=self.login)
         self.login_button.grid(row=5, column=0, columnspan=2, padx=10, pady=5)
 
-    # Création de l'entrée texte
+        # Centrage des colonnes
+        self.login_frame.grid_columnconfigure(0, weight=1)
+        self.login_frame.grid_columnconfigure(1, weight=1)
+
+        # Réactivité de l'interface
+        self.make_responsive()
+
+    def make_responsive(self):
+        # Pour rendre la fenêtre responsive, les lignes doivent s'adapter au redimensionnement de la fenêtre
+        for row in range(6):
+            self.login_frame.grid_rowconfigure(row, weight=1)
+
     def create_entry(self, parent, default_text, **kwargs):
         # Zones de textes
-        entry = tk.Entry(parent, width=30, **kwargs)
+        entry = ctk.CTkEntry(parent, width=100, **kwargs)
         entry.default_text = default_text
         entry.insert(0, entry.default_text)
-        entry.config(fg='grey')
         entry.bind("<FocusIn>", lambda event, entry=entry: self.entry_click_delete(event, entry))
         entry.bind("<FocusOut>", lambda event, entry=entry: self.entry_leave(event, entry))
         return entry
 
-     # Efface le texte par défaut lorsqu'un champ de saisie est cliqué et change la couleur du texte
     def entry_click_delete(self, event, entry):
         if entry.get() == entry.default_text:
             entry.delete(0, tk.END)
-            entry.config(fg='black')
+            entry.configure(fg='black')
 
-    # Rétablit le texte par défaut si le champ de saisie est vide lorsqu'il perd le focus, et change la couleur du texte
-    def entry_click_restore(self, event, entry):
+    def entry_leave(self, event, entry):
         if not entry.get():
             entry.insert(0, entry.default_text)
-            entry.config(fg='grey')
+            entry.configure(fg='grey')
 
-    # Gère l'action lorsque le bouton "Create account" est cliqué
     def create_account(self):
         print("Create account in process")
 
-    # Gère l'action lorsque le bouton "Login" est cliqué
     def login(self):
         print("Login in process")
 
