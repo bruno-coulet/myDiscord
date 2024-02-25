fce8768784a21a2eb55b17c42e17ca8f05311ab0
Cyril GENISSON <144389820+cyril-genisson@users.noreply.github.com>
2024-02-25 22:13:32 +0100

    PEP compliance for audio.py

--- audio.py
+++ audio.py
@@ -21,19 +21,20 @@ def record(duration, samplerate=44100, channels=2):
     :param channels: number of channels
     :return: numpy array with audio data
     """
-    print("Recording for 10s")
+    print(f"Recording for {duration}s")
     new_record = sd.rec(int(duration * samplerate), samplerate=samplerate, channels=channels)
     sd.wait()
     return new_record
 
-def play(recording, samplerate=44100):
+
+def play(records, samplerate=44100):
     """
     Plays recording
-    :param recording:
+    :param records:
     :param samplerate:
     :return: None
     """
-    sd.play(recording, samplerate=samplerate)
+    sd.play(records, samplerate=samplerate)
     sd.wait()
 
 
@@ -43,4 +44,3 @@ if __name__ == "__main__":
     # This will convert the NumPy array to an audio
     # file with the given sampling frequency
     write("recording0.wav", 44100, recording)
-
